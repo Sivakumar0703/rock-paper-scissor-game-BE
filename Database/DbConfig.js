e08dd255058583177261d1f81ee1dbca2a537b6d
sivakumar0703 <ssivakumar358@gmail.com>
2024-10-16 13:10:20 +0530

    delted commented code

--- Database/DbConfig.js
+++ Database/DbConfig.js
@@ -2,15 +2,7 @@ import pg from 'pg';
 import dotenv from "dotenv";
 
 dotenv.config();
-const { Pool , Client } = pg
- 
-// const pool = new Pool({
-//   host: process.env.HOST,
-//   user: process.env.USER_NAME,
-//   password:process.env.DB_PASSWORD,
-//   port:process.env.PORT,
-//   database:process.env.DB_NAME 
-// })
+const { Client } = pg
 
 const client = new Client({
   host: process.env.HOST,
@@ -28,20 +20,4 @@ client.connect((error) => {
   }
 })
 
-
-
-// pool.connect((error,client,release) => {
-//   if(error){
-//     return console.log("Error in Connecting DB",error);
-//   }
-//   client.query('SELECT NOW()', (err,result) => {
-//     release()
-//     if(err){
-//       return console.log("Error in executiong query")
-//     }
-//     console.log("Database connected");
-//   })
-// })
-
 export default client
-// export default pool
