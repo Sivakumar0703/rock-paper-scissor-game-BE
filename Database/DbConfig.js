61fa036cc751927395c172d0db4f3bee09f8d86e
sivakumar0703 <ssivakumar358@gmail.com>
2024-10-24 14:55:34 +0530

    connected with awd-rds-postgres

--- Database/DbConfig.js
+++ Database/DbConfig.js
@@ -5,7 +5,14 @@ dotenv.config();
 const { Client } = pg
 
 const client = new Client({
-  connectionString:process.env.DB_CONNECTION_URL
+  host:process.env.DB_HOST,
+  user:process.env.DB_USERNAME,
+  password:process.env.DB_PASSWORD,
+  port:process.env.DB_PORT,
+  database:process.env.DB_NAME,
+  ssl:{
+  rejectUnauthorized:false
+  }
 })
 
 client.connect((error) => {
